69d01701eda5a21a1773cd33128fdcbf7bf66c51
axeisghost <wjx941218@gmail.com>
2015-01-18 20:22:48 -0500

    Junxian Wu(axeisghost): Person1.java changed

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -30,8 +30,7 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
+		return input.substring(2) + input.charAt(0) + input.charAt(1);
 	}
 	
 	/**
