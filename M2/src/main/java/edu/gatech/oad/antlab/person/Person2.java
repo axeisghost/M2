2b46c212214e0dc643f2e175cbca68ef408e0eca
Zhao, Yuhui <yzhao343@gatech.edu>
2015-01-21 20:32:42 -0500

    yuhui finished person2

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -1,5 +1,6 @@
 package edu.gatech.oad.antlab.person;
 import java.util.Random;
+import java.util.ArrayList;
 
 /**
  *  A simple class for person 2
@@ -13,42 +14,49 @@ import java.util.Random;
 public class Person2 {
     /** Holds the persons real name */
     private String name;
-	 	/**
-	 * The constructor, takes in the persons
-	 * name
-	 * @param pname the person's real name
-	 */
-	 public Person2(String pname) {
-	   name = pname;
-	 }
-	/**
-	 * This method should take the string
-	 * input and return its characters in
-	 * random order.
-	 * given "gtg123b" it should return
-	 * something like "g3tb1g2".
-	 *
-	 * @param input the string to be modified
-	 * @return the modified string
-	 */
-	private String calc(String input) {
+        /**
+     * The constructor, takes in the persons
+     * name
+     * @param pname the person's real name
+     */
+     public Person2(String pname) {
+       name = pname;
+     }
+    /**
+     * This method should take the string
+     * input and return its characters in
+     * random order.
+     * given "gtg123b" it should return
+     * something like "g3tb1g2".
+     *
+     * @param input the string to be modified
+     * @return the modified string
+     */
+    private static String calc(String input) {
         int length = input.length();
         char[] charArray = new char[length];
-        for (int i = 0; i < length; i--) {
-            charArray[i] = input.charAt(i);
+        ArrayList<String> arr = new ArrayList<>();
+        for (int i = 0; i < length; i++) {
+            arr.add(Character.toString(input.charAt(i)));
         }
         Random rand = new Random();
-	  return null;
-	}
-	/**
-	 * Return a string rep of this object
-	 * that varies with an input string
-	 *
-	 * @param input the varying string
-	 * @return the string representing the
-	 *         object
-	 */
-	public String toString(String input) {
-	  return name + calc(input);
-	}
+        StringBuilder res = new StringBuilder();
+        while(!arr.isEmpty()) {
+            res = res.append(arr.remove(rand.nextInt(arr.size())));
+        }
+
+      return res.toString();
+    }
+    /**
+     * Return a string rep of this object
+     * that varies with an input string
+     *
+     * @param input the varying string
+     * @return the string representing the
+     *         object
+     */
+    public String toString(String input) {
+      return name + calc(input);
+    }
 }
+
